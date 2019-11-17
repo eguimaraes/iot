b212b4c8d6e7f4aa86af630a0d356b3e63a3edc6
edvaldo guimaraes <edvaldoGUIMARAES2013@hotmail.com>
2019-11-17 17:20:44 -0300

    Codigo Otimizado

--- arduinoExBasico/arduinoExBasico.ino
+++ arduinoExBasico/arduinoExBasico.ino
@@ -1,33 +1,37 @@
 int EstadoBotao1 = 0;
+int inPin=7;
+int val=1 ;
+
 
 void setup() {
 
 
- 
-  for(int x=2;x<=5; x++){   
+ pinSetupIn(inPin);
+  for(int x=2;x<=6; x++){   
     
   pinSetupOut(x);            
     
     }
-  
-  
+ 
+val=digitalRead(inPin);
 }
 
 void loop() {
 
-  
-
-
 
   
   for(int x=2;x<=5; x++){
+
+val=digitalRead(inPin);
     
+    if (val==HIGH){
     
     piscaLed(x,400);
-            
+    }       
   
 
 }
+
 }
 
 void piscaLed(int pin, int tempo){
